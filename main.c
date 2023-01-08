12de64fb3d67bee0f20ffc2a282eb44772735fc4
besufikad <07yilmabese@gmail.com>
2023-01-08 13:44:35 +0300

    cache

--- main.c
+++ main.c
@@ -3,21 +3,20 @@
 //#include(shell.h)
 #include<time.h>
 
-void main_memory();
-void direct(int *cache , int length);
-void associative();
-void set();
+
+
+
 typedef struct
 {
 
         int memory[32];
     int *pc;
 
-}direct1 , associative1 , set1;
-
+}direct1 ;
 
 int main()
 {
+
 
     int choice;
      printf("############################\n");
@@ -33,18 +32,20 @@ int main()
       switch(choice)
       {
       case 1:
-          //direct();
+          //direct_mapping();
           break;
       case 2:
-        associative();
+        //associative();
         break;
       case 3:
-        set();
+        //set();
         break;
       case 4:
         main_memory();
         break;
       case 5:
+
+
         return 0;
         default:
             printf("sorry invalid choice\n");
@@ -61,69 +62,42 @@ int main()
 
 
 
-void main_memory()
+direct1 main_memory()
 {
 // sample memory unit which has 21 IAS instructions and generated words in hexadecimal form
 
-direct1 main;
+direct1 copy;
 
   int array[]= {00001010 ,00001001 ,00100001 ,00000001};
 
   printf("ADDRESS       WORDS/INSTRUCTIONS\n");
   for (int i = 0; i < 4; i++)
   {
-      main.memory[i] = array[i];
-      main.pc = &main.memory[i];
-   printf("0x%p         0x%x\n" , main.pc , main.memory[i]);
+      copy.memory[i] = array[i];
+      copy.pc = &copy.memory[i];
+   printf("0x%p         0x%x\n" , copy.pc , copy.memory[i]);
   }
 
  for (int j = 4; j < 32; j++)
  {
      srand(time(NULL));
-     main.memory[j] = rand() / j;
-     main.pc = &main.memory[j];
-   printf("0x%p        0x%x\n" ,main.pc , main.memory[j]);
+     copy.memory[j] = rand() / j;
+     copy.pc = &copy.memory[j];
+   printf("0x%p        0x%x\n" ,copy.pc , copy.memory[j]);
 
 
    }
-  return;
+  return copy;
 }
 
 
-/*void *direct(int *cache , int length)
+/*void direct_mapping(int *cache , int length)
 {
-    int tag;
-    int *c = malloc(length * sizeof(int));
-    printf("Choose(1 or 2):\n");
-    printf("Instruction or Randomly Generated Words\n");
-    int select;
-    scanf("%d" , &select);
-    if (select == 1)
-    {
-        for (int i = 0; i < length; i++)
-        {
-            cache[i]= memory[i];
-            tag = pc;
 
-        }
-    }
-    else
-    {
-        for (int j = 21; j < 32; j++)
-        {
-            cache[j] = memory2[j];
-            tag = pc2;
-        }
-    }
+    printf("%d" , copy.memory[3]);
+
 
 
 }*/
-void associative()
-{
 
-}
-void set()
-{
-
-}
 
